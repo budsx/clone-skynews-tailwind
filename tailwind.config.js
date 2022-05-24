8e3b3c3b00ece61a3eb59abda7d55a9d419ab8e5
Budi <budiskom99@gmail.com>
2022-05-24 11:42:26 +0700

    skynews clone initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        Plus: ['Plus Jakarta Sans', 'sans-serif'],
+        Bebas: ['Bebas Neue'],
+      },
+      backgroundColor: {
+        base: ['#252525'],
+      },
+      borderColor: {
+        base: ['#292929'],
+      },
+    },
+  },
+  plugins: [],
+}
